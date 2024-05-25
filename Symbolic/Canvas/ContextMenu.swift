9ef89c747761c96a1bed41fd7ad70a12bf7a9e3e
Yaindrop <yuan_dian@live.com>
2024-05-25 10:52:50 +0800

    wip

--- Symbolic/Canvas/ContextMenu.swift
+++ Symbolic/Canvas/ContextMenu.swift
@@ -1,6 +1,27 @@
 import Foundation
 import SwiftUI
 
+enum ContextMenuData {
+    struct PathNode {
+        let pathId: UUID, nodeId: UUID
+    }
+
+    struct PathEdge {
+        let pathId: UUID, fromNodeId: UUID
+    }
+
+    struct Path {
+        let pathId: UUID
+    }
+
+    struct Selection {
+        let pathIds: [UUID]
+    }
+}
+
+class ContextMenuStore: Store {
+}
+
 struct ContextMenu: View {
     var onDelete: (() -> Void)?
 
@@ -10,7 +31,7 @@ struct ContextMenu: View {
         HStack {
             Image(systemName: "rectangle.3.group")
             Divider()
-            Button("", systemImage: "trash", role: .destructive) { onDelete?() }
+            Button(role: .destructive) { onDelete?() } label: { Image(systemName: "trash") }
         }
         .padding(12)
         .background(.thickMaterial)

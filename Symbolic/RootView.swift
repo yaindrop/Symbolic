5690c7c2c045a7327d644eb21d6bd19f411640f6
Yaindrop <yuan_dian@live.com>
2024-06-22 23:43:24 +0800

    wip

--- Symbolic/RootView.swift
+++ Symbolic/RootView.swift
@@ -24,70 +24,113 @@ struct RootView: View, TracedView, SelectorHolder {
     }}
 }
 
-struct DocumentsView: View, TracedView, SelectorHolder {
-    class Selector: SelectorBase {
-        override var syncNotify: Bool { true }
-        @Selected({ global.panel.movingPanelMap }) var movingPanelMap
-    }
-
-    @SelectorWrapper var selector
-
+struct DocumentsView: View, TracedView {
     @State private var fileTree: FileTree?
 
     var body: some View { trace {
-        setupSelector {
-            content
-                .onAppear {
-                    Task {
-                        fileTree = await listFiles()
-                    }
+        content
+            .onAppear {
+                Task {
+                    fileTree = await listFiles()
+                    print("fileTree", fileTree)
                 }
-        }
+            }
     } }
 }
 
 extension DocumentsView {
+    @ViewBuilder var content: some View {
+        if let fileTree {
+            DirectoryView(fileTree: fileTree, url: fileTree.root)
+        } else {
+            Text("Loading")
+        }
+    }
+}
+
+struct DirectoryView: View, TracedView {
+    @Environment(\.dismiss) private var dismiss
+
+    let fileTree: FileTree
+    let url: URL
+
+    var body: some View { trace {
+        content
+    } }
+}
+
+extension DirectoryView {
     var data: [FileTree.Entry] {
-        guard let fileTree else { return [] }
-        return fileTree.dirToEntries[fileTree.root] ?? []
+        guard let entries = fileTree.dirToEntries[url] else { return [] }
+        return entries.sorted {
+            if $0.isDirectory == $1.isDirectory {
+                return $0.url.lastPathComponent < $1.url.lastPathComponent
+            }
+            return $0.isDirectory
+        }
     }
 
     var content: some View {
-        ZStack {
-            ScrollView {
-                VStack {
-                    Button("Canvas!") { global.root.update(showCanvas: !global.root.showCanvas) }
-//                        .fullScreenCover(isPresented: $showCanvas) {
-//                            CanvasView()
-//                        }
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], spacing: 20) {
-                        ForEach(data, id: \.url) { entry in
-                            VStack(spacing: 0) {
-                                HStack {
-                                    Image(systemName: "questionmark.app.dashed")
-                                        .font(.title)
-                                }
-                                .frame(maxWidth: .infinity, maxHeight: 100, alignment: .center)
-                                .background(.background.tertiary)
-                                Divider()
-                                Text(entry.url.lastPathComponent)
-                                    .frame(maxWidth: .infinity, maxHeight: 50, alignment: .center)
-                                    .background(entry.isDirectory ? .blue : .gray)
-                                    .foregroundColor(.white)
-                                    .font(.headline)
-                            }
-                            .frame(width: 150, height: 150, alignment: .center)
-                            .clipRounded(radius: 12)
-                            .onTapGesture {
-                                global.root.open(url: entry.url)
-                            }
-                        }
+        ScrollView {
+            VStack {
+                Button("Canvas!") { global.root.update(showCanvas: !global.root.showCanvas) }
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], spacing: 20) {
+                    ForEach(data, id: \.url) {
+                        EntryCard(fileTree: fileTree, entry: $0)
                     }
                 }
-                .padding()
             }
-            .navigationTitle("Documents")
+            .padding()
         }
+        .navigationTitle(url.lastPathComponent)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button { dismiss() } label: { Image(systemName: "chevron.left") }
+                    .disabled(url == fileTree.root)
+            }
+        }
+    }
+}
+
+struct EntryCard: View, TracedView {
+    let fileTree: FileTree
+    let entry: FileTree.Entry
+
+    var body: some View { trace {
+        content
+    } }
+}
+
+extension EntryCard {
+    @ViewBuilder var content: some View {
+        if entry.isDirectory {
+            NavigationLink { DirectoryView(fileTree: fileTree, url: entry.url) } label: { card }
+                .tint(.label)
+        } else {
+            card
+                .onTapGesture {
+                    global.root.open(url: entry.url)
+                }
+        }
+    }
+
+    var card: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Image(systemName: entry.isDirectory ? "folder.fill" : "doc.fill")
+                    .font(.system(size: 48))
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100, alignment: .center)
+            .background(.background.tertiary)
+            Divider()
+            Text(entry.url.lastPathComponent)
+                .frame(maxWidth: .infinity, maxHeight: 50, alignment: .center)
+                .background(entry.isDirectory ? .blue : .gray)
+                .foregroundColor(.white)
+                .font(.headline)
+        }
+        .frame(width: 150, height: 150, alignment: .center)
+        .clipRounded(radius: 12)
     }
 }
 
@@ -95,20 +138,11 @@ enum SidebarType: CaseIterable, SelfIdentifiable {
     case document, panel
 }
 
-struct SidebarView: View, TracedView, SelectorHolder {
-    class Selector: SelectorBase {
-        override var syncNotify: Bool { true }
-        @Selected({ global.panel.movingPanelMap }) var movingPanelMap
-    }
-
-    @SelectorWrapper var selector
-
+struct SidebarView: View, TracedView {
     @State private var sidebarType: SidebarType = .document
 
     var body: some View { trace {
-        setupSelector {
-            content
-        }
+        content
     } }
 }
 
@@ -144,10 +178,15 @@ extension FileTree {
         var dirToEntries: [URL: [Entry]] = [:]
         let enumerator = FileManager.default.enumerator(atPath: root.path)
         while let relative = enumerator?.nextObject() as? String {
-            let url = root.appending(path: relative)
             guard let fType = enumerator?.fileAttributes?[FileAttributeKey.type] as? FileAttributeType,
                   fType == .typeRegular || fType == .typeDirectory else { continue }
             let isDirectory = fType == .typeDirectory
+            let url = root.appending(path: relative, directoryHint: isDirectory ? .isDirectory : .notDirectory)
+            let name = url.lastPathComponent
+            if name.hasPrefix(".") {
+                continue
+            }
+
             let parent = url.deletingLastPathComponent()
             var peers = dirToEntries.getOrSetDefault(key: parent, [])
             peers.append(.init(url: url, isDirectory: isDirectory))

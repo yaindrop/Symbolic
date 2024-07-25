3c8714d8f012ec45c1b1b099e1297288265b027c
Yaindrop <yuan_dian@live.com>
2024-07-25 11:17:33 +0800

    wip

--- Symbolic/Panel/DataInput/PositionPicker.swift
+++ Symbolic/Panel/DataInput/PositionPicker.swift
@@ -13,50 +13,65 @@ struct PositionPicker: View {
 
     @State private var showNumpadX: Bool = false
     @State private var showNumpadY: Bool = false
+}
 
-    private var content: some View {
+// MARK: private
+
+private extension PositionPicker {
+    var content: some View {
         HStack(spacing: 0) {
             Image(systemName: "arrow.up.right.square")
                 .padding(.leading, 6)
-
-            Button {
-                showNumpadX.toggle()
-            } label: {
-                Text(position.x.decimalFormatted())
-                    .padding(.horizontal, 6)
-                    .frame(maxHeight: .infinity)
-            }
-            .tint(.label)
-
+            buttonX
             Divider()
                 .padding(.vertical, 6)
-
-            Button {
-                showNumpadY.toggle()
-            } label: {
-                Text(position.y.decimalFormatted())
-                    .padding(.horizontal, 6)
-                    .frame(maxHeight: .infinity)
-            }
-            .tint(.label)
+            buttonY
         }
         .font(.footnote.monospacedDigit())
         .frame(height: 32)
         .background(Color.tertiarySystemBackground)
         .clipRounded(radius: 6)
-        .portal(isPresented: $showNumpadX, isModal: true, align: .topInnerTrailing) {
-            NumpadPortal(initialValue: position.x, configs: .init(label: "x")) {
-                onChange?(position.with(x: $0))
-            } onDone: {
-                onDone?(position.with(x: $0))
-            }
+        .portal(isPresented: $showNumpadX, isModal: true, align: .topInnerTrailing) { numpadX }
+        .portal(isPresented: $showNumpadY, isModal: true, align: .topInnerTrailing) { numpadY }
+    }
+
+    var buttonX: some View {
+        Button {
+            showNumpadX.toggle()
+        } label: {
+            Text(position.x.decimalFormatted())
+                .padding(.horizontal, 6)
+                .frame(maxHeight: .infinity)
         }
-        .portal(isPresented: $showNumpadY, isModal: true, align: .topInnerTrailing) {
-            NumpadPortal(initialValue: position.y, configs: .init(label: "y")) {
-                onChange?(position.with(y: $0))
-            } onDone: {
-                onDone?(position.with(y: $0))
-            }
+        .tint(.label)
+        .opacity(showNumpadX ? 0.5 : 1)
+    }
+
+    var buttonY: some View {
+        Button {
+            showNumpadY.toggle()
+        } label: {
+            Text(position.y.decimalFormatted())
+                .padding(.horizontal, 6)
+                .frame(maxHeight: .infinity)
+        }
+        .tint(.label)
+        .opacity(showNumpadY ? 0.5 : 1)
+    }
+
+    var numpadX: some View {
+        NumpadPortal(initialValue: position.x, configs: .init(label: "x")) {
+            onChange?(position.with(x: $0))
+        } onDone: {
+            onDone?(position.with(x: $0))
+        }
+    }
+
+    var numpadY: some View {
+        NumpadPortal(initialValue: position.y, configs: .init(label: "y")) {
+            onChange?(position.with(y: $0))
+        } onDone: {
+            onDone?(position.with(y: $0))
         }
     }
 }
